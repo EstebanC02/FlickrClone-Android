523909739d055252167dfa72210928610e957f2e
estebanmoncaleano <estebancastiblanco02@gmail.com>
2017-05-19 19:13:08 -0500

    Design of the MainActivity

--- app/src/main/java/com/estebanmoncaleano/flickrclone/MainActivity.java
+++ app/src/main/java/com/estebanmoncaleano/flickrclone/MainActivity.java
@@ -1,7 +1,12 @@
 package com.estebanmoncaleano.flickrclone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.estebanmoncaleano.flickrclone.utils.FontHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FontHelper.setCustomTypeface(findViewById(R.id.rl_main_view));
+
+        Spinner spinner = (Spinner) findViewById(R.id.sp_type_search);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.search_type_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void onSearch(View view) {
     }
 }
